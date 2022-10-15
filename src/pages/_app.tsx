3dd8cc936894486e46f901fcba1d9d4105dd64b7
BeWoler <bewoler.del@gmail.com>
2022-10-15 13:46:16 +0300

    added icons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app';
 import Layout from '@/hoc/layout/Layout';
-import '@/assets/styles/globals.sass';
+import '../../public/assets/styles/globals.sass'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,24 @@ const Home: NextPage = () => {
       <Head>
         <title>Apple Shop</title>
         <meta name="description" content="Apple Shop" />
-        {/* <link rel="icon" href="/favicon.ico" /> */}
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
       </Head>
       <main>Main</main>
     </div>
