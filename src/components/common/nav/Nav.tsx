6d58396c7735f433ef550d51970952895fa8819b
BeWoler <bewoler.del@gmail.com>
2022-10-16 12:24:34 +0300

    added nav & refactored header

---
+++ src/components/common/nav/Nav.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+import { FC } from 'react';
+import styles from './styles/nav.module.sass';
+
+const navData = [
+  { id: 1, title: 'О продукции', href: '' },
+  { id: 2, title: 'Каталог', href: '' },
+  { id: 3, title: 'Акции', href: '' },
+  { id: 4, title: 'Доставка&Оплата', href: '' },
+];
+
+const Nav: FC = (): JSX.Element => {
+  return (
+    <nav>
+      <ul className={styles.nav__menu}>
+        {navData?.map((item) => {
+          return (
+            <li key={item.id} className={styles.nav__menu__item}>
+              <Link href={item.href}>
+                <a title={item.title}>{item.title}</a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
